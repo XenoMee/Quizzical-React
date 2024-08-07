f4049a8e6c785a959bca6c11ee0509a983d1068f
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-07 17:00:20 +0300

    create StartScreen component structure

---
+++ src/Components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StartScreen = () => {
+  <div className='grid gap-4'>
+    <div>
+      <h1 className='h1'>Quizzical</h1>
+      <p>Ready to test your knowledge?</p>
+    </div>
+    <button className='button font-medium'>Start Quiz</button>
+  </div>;
+};
+
+export default StartScreen;
