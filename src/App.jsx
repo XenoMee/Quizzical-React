512f9a3e5f920ca5e484ac7e8bf4db53238639e2
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-07 17:37:11 +0300

    render Start and Quiz screen components conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
 import StartScreen from './Components/StartScreen';
 
 const App = () => {
+  const [quizActive, setQuizActive] = useState(false);
+
+  const toggleQuiz = () => {
+    setQuizActive((prevQuiz) => !prevQuiz);
+  };
+
   return (
     <>
       <main className='min-h-svh background-blobs grid place-content-center'>
-        <StartScreen />
+        {quizActive ? <QuizScreen /> : <StartScreen activateQuiz={toggleQuiz} />}
       </main>
     </>
   );
--- src/Components/StartScreen.jsx
+++ src/Components/StartScreen.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { act } from 'react';
 
-const StartScreen = () => {
+const StartScreen = ({ activateQuiz }) => {
   return (
     <div className='grid gap-10 text-center'>
       <div>
         <h1 className='h1 mb-3'>Quizzical</h1>
         <p>Ready to test your knowledge?</p>
       </div>
-      <button className='button font-medium justify-self-center'>Start Quiz</button>
+      <button className='button font-medium justify-self-center' onClick={activateQuiz}>
+        Start Quiz
+      </button>
     </div>
   );
 };
