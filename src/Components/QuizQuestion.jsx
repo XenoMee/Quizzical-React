d97b9755b4654fe8695be1df380d5442fb915930
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-08 13:46:16 +0300

    place radio input inside label field

--- src/Components/QuizQuestion.jsx
+++ src/Components/QuizQuestion.jsx
@@ -7,10 +7,10 @@ const QuizQuestion = ({ id, question, answers }) => (
       <legend className='h2 my-2'> {decode(question)}</legend>
       <div className='flex xxs:flex-col sm:flex-row justify-between items-center'>
         {answers[id].map((answer, index) => (
-          <div key={index} className='flex items-center gap-2 text-center sm:text-left'>
-            <label htmlFor={`question${id + 1}`}>{decode(answer)}</label>
+          <label key={index} role='button'>
             <input type='radio' name={`question${id + 1}`} value={decode(answer)} />
-          </div>
+            {decode(answer)}
+          </label>
         ))}
       </div>
     </fieldset>
