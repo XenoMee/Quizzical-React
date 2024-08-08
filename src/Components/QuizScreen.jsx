902a5a0960aa8cf75a99815d4ca60677e3290932
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-08 12:53:22 +0300

    create new array with all question answers

--- src/Components/QuizScreen.jsx
+++ src/Components/QuizScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import QuizQuestion from './QuizQuestion';
+import insertRandomly from '../Utilities/insertRandomItem';
 
 const QuizScreen = ({ quizData }) => {
+  const options = quizData.results.map((result) => {
+    const wrong_answers = [...result.incorrect_answers];
+
+    insertRandomly(wrong_answers, result.correct_answer);
+
+    return wrong_answers;
+  });
+
   return (
     <div className='px-8'>
       <ul className='grid gap-2'>
         {quizData.results.map((result, index) => (
-          <QuizQuestion key={index} question={result.question} />
+          <QuizQuestion key={index} id={index} question={result.question} />
         ))}
       </ul>
     </div>
