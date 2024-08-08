9ebb683ad54c61d065105c6034ef3240f856f3b6
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-08 13:36:46 +0300

    render question options

--- src/Components/QuizQuestion.jsx
+++ src/Components/QuizQuestion.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { decode } from 'html-entities';
 
-const QuizQuestion = ({ question }) => (
-  <li className='pb-2 border-b-[1px] border-blue-25'>
-    <h2 className='h2'>{decode(question)}</h2>
+const QuizQuestion = ({ id, question, answers }) => (
+  <li className='pb-4 border-b-[1px] border-blue-25'>
+    <fieldset>
+      <legend className='h2 my-2'> {decode(question)}</legend>
+      <div className='flex xxs:flex-col sm:flex-row justify-between items-center'>
+        {answers[id].map((answer, index) => (
+          <div key={index} className='flex items-center gap-2 text-center sm:text-left'>
+            <label htmlFor={`question${id + 1}`}>{decode(answer)}</label>
+            <input type='radio' name={`question${id + 1}`} value={decode(answer)} />
+          </div>
+        ))}
+      </div>
+    </fieldset>
   </li>
 );
 
