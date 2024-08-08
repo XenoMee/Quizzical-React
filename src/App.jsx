889f59c309eda3bb34eedd82e0ed685216c19f28
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-08 10:49:49 +0300

    fetch data from open trivia database API to State

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import StartScreen from './Components/StartScreen';
+import QuizScreen from './Components/QuizScreen';
 
 const App = () => {
   const [quizActive, setQuizActive] = useState(false);
+  const [quizData, setQuizData] = useState('');
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple')
+      .then((response) => response.json())
+      .then((data) => setQuizData(data));
+  }, []);
 
   const toggleQuiz = () => {
     setQuizActive((prevQuiz) => !prevQuiz);
@@ -11,7 +19,7 @@ const App = () => {
   return (
     <>
       <main className='min-h-svh background-blobs grid place-content-center'>
-        {quizActive ? <QuizScreen /> : <StartScreen activateQuiz={toggleQuiz} />}
+        {quizActive ? <QuizScreen quizData={quizData} /> : <StartScreen activateQuiz={toggleQuiz} />}
       </main>
     </>
   );
