4f35a63a37af0772eb0d45c9af60361f08fb107c
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-08 09:55:59 +0300

    create quiz screen component

---
+++ src/Components/QuizScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const QuizScreen = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <h2>How would you say goodbye in Spanish?</h2>
+        </li>
+        <li>
+          <h2>What is the hottest planet in our Solar System?</h2>
+        </li>
+        <li>
+          <h2>In which country was the Caesar salad invented?</h2>
+        </li>
+        <li>
+          <h2>How many hearts does an octopus have?</h2>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default QuizScreen;
