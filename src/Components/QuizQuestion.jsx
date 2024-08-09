d49bdbb64a19cb40092a6b65640eeb78d233b374
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-08-09 12:42:36 +0300

    update code to current quiz data

--- src/Components/QuizQuestion.jsx
+++ src/Components/QuizQuestion.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { decode } from 'html-entities';
 
 const QuizQuestion = ({ id, question, answers }) => (
   <li className='pb-4 pt-2 border-b-[1px] border-blue-25'>
     <fieldset>
-      <legend className='h2 mb-4'> {decode(question)}</legend>
+      <legend className='h2 mb-4'> {question}</legend>
       <div className='flex text-center xxs:flex-col xxs:gap-4 sm:flex-row sm:justify-between sm:items-center'>
-        {answers[id].map((answer, index) => (
+        {answers.map((answer, index) => (
           <label
             key={index}
             role='button'
             className='label-button hover:bg-blue-600 hover:text-white-50 transition delay-100 ease-in-out '
           >
-            <input type='radio' name={`question${id + 1}`} value={decode(answer)} className='hidden' />
-            <span>{decode(answer)}</span>
+            <input type='radio' name={`question${id + 1}`} value={answer} className='hidden' />
+            <span>{answer}</span>
           </label>
         ))}
       </div>
--- src/Components/QuizScreen.jsx
+++ src/Components/QuizScreen.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import QuizQuestion from './QuizQuestion';
-import insertRandomly from '../Utilities/insertRandomItem';
 
 const QuizScreen = ({ quizData }) => {
-  const options = quizData.results.map((result) => {
-    const wrong_answers = [...result.incorrect_answers];
-
-    insertRandomly(wrong_answers, result.correct_answer);
-
-    return wrong_answers;
-  });
-
   return (
     <div className='p-8'>
       <form action='' className='grid gap-10'>
         <ul className='grid gap-2'>
-          {quizData.results.map((result, index) => (
-            <QuizQuestion key={index} id={index} question={result.question} answers={options} />
+          {quizData.map((item, index) => (
+            <QuizQuestion key={index} id={index} question={item.question} answers={item.answers} />
           ))}
         </ul>
 
